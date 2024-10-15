e0e6e63330c5c8b131a1fbc8b1853c5d0f163a55
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-15 13:54:07 +0200

    balz

--- script/main.py
+++ script/main.py
@@ -2,14 +2,16 @@ from .search_website_url import search_website_url
 from .cbe_formatting import cbe_formatting
 from .cbe_screening import cbe_screening
 import time
-import pandas as pd
 
 def main(files):
+
     # Record the start time
     start_time = time.time()
 
     # Format the input files
     startup_data = cbe_formatting(files)
+
+    startup_data = startup_data[:100]
 
     # Print the number of filtered rows based on NACE code
     print(f"Nace Code Filter: {len(startup_data)}")
@@ -18,24 +20,13 @@ def main(files):
     if startup_data.empty:
         return startup_data
 
-    # Split startup_data into chunks of 50
-    chunk_size = 50
-    chunks = [startup_data[i:i + chunk_size] for i in range(0, len(startup_data), chunk_size)]
-
-    # Initialize an empty DataFrame to store the results
-    all_data = pd.DataFrame()
-
-    # Process each chunk
-    for idx, chunk in enumerate(chunks):
-        print(f"Processing chunk {idx + 1}/{len(chunks)}")
-        startup_data_chunk = cbe_screening(chunk)
-        all_data = pd.concat([all_data, startup_data_chunk], ignore_index=True)
-    startup_data = all_data
+    # Apply CBE screening to the data
+    startup_data = cbe_screening(startup_data)
 
     # Print the number of filtered rows after CBE screening
     print(f"CBE Screen Filter: {len(startup_data)}")
 
-    # Check again if all_data is empty, return it if true
+    # Check again if startup_data is empty, return it if true
     if startup_data.empty:
         return startup_data
 
